5ef2e55138c0cde67b9608bb3b215302091eb57e
blaze <blaze@kakaocorp.com>
2023-08-03 10:42:52 +0900

    Update. 로그아웃 기능 개발 Update. context api 이용해서 token 값 저장로직 구현

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,9 +1,12 @@
+import {Alert} from 'react-native';
 import AuthContent from '../components/Auth/AuthContent';
 import {useState} from "react";
 import {createUser} from "../util/auth";
 import LoadingOverlay from "../components/ui/LoadingOverlay";
+import {useNavigation} from "@react-navigation/native";
 
 function SignupScreen() {
+  const navigator = useNavigation();
   const [isAuthenticating, setIsAuthenticating] = useState(false);
   const signupHandler = async ({email, password}) => {
     setIsAuthenticating(true);
@@ -11,11 +14,20 @@ function SignupScreen() {
     try{
       const res = await createUser({email, password});
       console.log('creater User', res);
+      Alert.alert('Done', 'SingUp Success', [
+        {
+          text: 'ok',
+          onPress: () => {
+            navigator.navigate('Login');
+            setIsAuthenticating(false);
+          },
+          style: 'ok',
+        },
+      ]);
     }catch (e){
-      console.error(e);
+      Alert.alert('Authentication failed!', 'Could not create user. Please try again later.')
+      setIsAuthenticating(false);
     }
-
-    setIsAuthenticating(false);
   }
 
   if(isAuthenticating){
--- util/auth.js
+++ util/auth.js
@@ -1,7 +1,14 @@
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 import {app} from '../firebase';
 export async function createUser(params) {
   const auth = getAuth(app);
 
-  return createUserWithEmailAndPassword(auth, params.email, params.password)
+  return await createUserWithEmailAndPassword(auth, params.email, params.password)
+}
+
+export async function login({email, password}){
+  const auth = getAuth(app);
+  console.log('login', email, password);
+
+  return await signInWithEmailAndPassword(auth, email, password);
 }
